afcad18efb574b7faed39e3bd336c0ce5ad989bd
Greg Hoelzer <ghoelzer@redhat.com>
2014-11-13 18:09:12 -0600

    Added Arguilian Junit test

---
+++ src/test/java/eaphelloworldv100/TestHelloWorldServlet.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package eaphelloworldv100;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.quickstarts.helloworld.HelloService;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+/**
+ * @author ghoelzer
+ *
+ */
+
+@RunWith(Arquillian.class)
+public class TestHelloWorldServlet {
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+            .addClass(HelloService.class)
+            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+    @Inject
+    HelloService greeter;
+
+    @Test
+    public void should_create_greeting() {
+        Assert.assertEquals("Hello Earthling!",
+            greeter.createHelloMessage("Earthling"));
+        System.out.println(greeter.createHelloMessage("Earthling"));
+    }
+
+}
