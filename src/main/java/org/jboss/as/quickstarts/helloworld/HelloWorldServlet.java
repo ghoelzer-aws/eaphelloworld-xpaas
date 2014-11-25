2158dc86cd713c9a5b8b275be70cb0a5f5654ceb
Greg Hoelzer <ghoelzer@redhat.com>
2014-11-25 14:48:58 -0600

    Merged branch with master update

--- src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
+++ src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
@@ -55,7 +55,7 @@ public class HelloWorldServlet extends HttpServlet {
       PrintWriter writer = resp.getWriter();
       writer.println(PAGE_HEADER);
       writer.println("<h1>" + helloService.createHelloMessage("World") + "</h1>");
-      writer.println("<h2>" + System.getProperty("os.name") + " " + System.getProperty("os.version") + "</h2>");
+      writer.println("<h2>" + System.getProperty("os.vendor") + System.getProperty("os.name") + " " + System.getProperty("os.version") + "</h2>");
       writer.println("<h2>" + "Java Version: " + System.getProperty("java.version") + "</h2>");
       writer.println(PAGE_FOOTER);
       writer.close();
